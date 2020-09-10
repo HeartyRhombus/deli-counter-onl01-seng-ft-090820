39def2157d2b62433dfb3decabaedd483953d1ef
Kayla Budzeak <kaylahmg@gmail.com>
2020-09-10 03:52:24 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -11,6 +11,7 @@ def line(number)
 end
 
 def take_a_number(katz_deli, name)
+  katz_deli.push(name)
   new_line = []
   katz_deli.each.with_index(1) do |name, index|
     new_line.push("Welcome, #{name}. You are number #{index} in line.")
