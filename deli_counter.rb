e02a294c90f4775b6f85335ddaa54f5d4b7ce96b
Kayla Budzeak <kaylahmg@gmail.com>
2020-09-10 03:29:48 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,6 +1,9 @@
 def line(number)
   if number = 0
     puts "The line is currently empty."
+  else
+    number.each.with_index(1) do |index, name|
+      puts "The line is currently: #{index}. #{name} "
   end
 
 end
